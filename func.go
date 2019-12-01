4e4440fce151efc5dc8e8033d0224060fa2e3e84
Rob <rtarbath@gmail.com>
2019-12-02 01:13:33 +1100

    made func element respect container interface

--- func.go
+++ func.go
@@ -1,5 +1,7 @@
 package tcui
 
+import "github.com/gdamore/tcell"
+
 //
 type Func struct {
 	F     func(Theme) Element
@@ -26,4 +28,65 @@ func (f *Func) Size() (int, int) {
 //
 func (f *Func) SetTheme(theme Theme) {
 	f.Theme = theme
+}
+
+//
+func (f *Func) GetFocusable() []Focusable {
+	var eles []Focusable
+
+	child := f.F(f.Theme)
+	if cont, ok := child.(Container); ok {
+		eles = cont.GetFocusable()
+	} else if focusable, ok := child.(Focusable); ok {
+		eles = []Focusable{focusable}
+	}
+
+	return eles
+}
+
+//
+func (f *Func) NextFocusable(current Focusable) Focusable {
+	eles := f.GetFocusable()
+
+	// if there are focusable
+	if len(eles) > 0 {
+		//find the next focusable
+		if current != nil {
+			var curIdx int
+
+			for i, e := range eles {
+				if e == current {
+					curIdx = i
+				}
+			}
+
+			if curIdx < len(eles)-1 {
+				return eles[curIdx+1]
+			}
+			return eles[0]
+
+		}
+		// if nothing is currently focused, return the first
+		return eles[0]
+
+	}
+	// if there are no focusable, return nil
+	return nil
+}
+
+//
+func (f *Func) FocusClicked(ev tcell.EventMouse) Focusable {
+	c := f.F(f.Theme)
+
+	if clickable, ok := c.(Clickable); ok {
+		clickable.HandleClick(ev)
+	}
+
+	if cont, ok := c.(Container); ok {
+		return cont.FocusClicked(ev)
+	} else if foc, ok := c.(Focusable); ok {
+		return foc
+	}
+
+	return nil
 }
